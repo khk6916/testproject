e368ed9d9a959651fb9ccb5351385f5817c21702
Hyunki Kim <khk6916@naver.com>
2020-10-08 16:09:47 +0900

    저장

---
+++ project_v1/Assets/Scripts/MapMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMove : MonoBehaviour
+{
+    public float moveSpeed;
+    Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+  
+    void Update()
+    {
+        tr.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ project_v1/Assets/Scripts/PlyerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    Transform tr;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            tr.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            tr.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+    }
+}
